f254bfb774e445d73b067a684c89888463428158
blackcode@DESKTOP-CC0RJLG <aqilblackcode@gmail.com>
2021-03-16 15:22:39 +0700

    praktikum 3 add new column custom login and regist

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,28 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                    <table class="table table-responsive">
+                        <tr>
+                            <th>Username</th>
+                            <th>:</th>
+                            <td>{{$data->username}}</td>
+                        </tr>
+                         <tr>
+                            <th>Name</th>
+                            <th>:</th>
+                            <td>{{$data->name}}</td>
+                        </tr>
+                         <tr>
+                            <th>Email</th>
+                            <th>:</th>
+                            <td>{{$data->email}}</td>
+                        </tr>
+                         <tr>
+                            <th>Cretaed At</th>
+                            <th>:</th>
+                            <td>{{$data->created_at}}</td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
